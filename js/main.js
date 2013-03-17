dae4eb302829e6197b9bc7b1004e28dd84d2127d
Adam Soffer <adam.soffer@digitalsurgeons.com>
2013-03-17 01:39:22 -0400

    timer

--- js/main.js
+++ js/main.js
@@ -15,7 +15,25 @@ Gumby.oldie(function() {
 
 // Document ready
 $(function() {
+  var timerId =
+  countdown(
+    new Date("April 27, 2013 06:00:00"),
+    function(ts) {
+      console.log(ts);
+      document.getElementById('meetupTimer').innerHTML = ts.toHTML("strong");
+    },
+    countdown.HOURS|countdown.MINUTES|countdown.SECONDS);
+    
+    var timerId =
+  countdown(
+    new Date("March 20, 2013 12:30:00"),
+    function(ts) {
+      console.log(ts);
+      document.getElementById('lunchTimer').innerHTML = ts.toHTML("strong");
+    },
+    countdown.HOURS|countdown.MINUTES|countdown.SECONDS);
 
-
+// later on this timer may be stopped
+//window.clearInterval(timerId);
 });
 
