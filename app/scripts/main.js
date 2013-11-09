5cad09b41400da15e9523ddcdf7630ebb90706fc
Kyle L. Jensen <kljensen@gmail.com>
2013-11-09 14:35:23 -0500

    Make developer/company live search look at all text, including name, bio, languages

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,20 +1,25 @@
 $(function() {
     
   var $grid = $('#grid');
+  if ($grid.length > 0) {
 
-  // initialize
-  $grid.shuffle({
-    itemSelector: '.brick-item',
-    columnWidth: 470
-  });
-      
-  $('.js-shuffle-search').on('keyup change', function() {
-    
-    var val = this.value.toLowerCase();
-   
-    $grid.shuffle('shuffle', function($el, shuffle) { 
-      var text = $.trim( $el.find('span.name').text() ).toLowerCase();
-      return text.indexOf(val) !== -1;
+    // Initialize element with shuffle plugin
+    $grid.shuffle({
+      itemSelector: '.brick-item',
+      columnWidth: 470
     });
-  });
+        
+    $('.js-shuffle-search').on('keyup change', function() {
+
+      // This is the value for which we're searching 
+      var val = this.value.toLowerCase();
+     
+      // See which elements match
+      $grid.shuffle('shuffle', function($el, shuffle) { 
+        var text = $.trim( $el.text() ).toLowerCase();
+        return text.indexOf(val) !== -1;
+      });
+    });
+    
+  };
 });
