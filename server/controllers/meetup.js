ab4b601f43b76fa5c7ccb37377962cbf06cf8fc2
Kyle L. Jensen <kljensen@gmail.com>
2013-11-14 05:59:23 -0500

    Cache the index page, it never changes.

--- server/controllers/meetup.js
+++ server/controllers/meetup.js
@@ -16,11 +16,23 @@ MeetupController.prototype.route = function()
 {
     var _this = this;
 
+    var cachedResponse = null;
+
     // Create new companies entry
     this.app.get('/', function(req, res)
     {
-        res.render('index.html', {
-            meetups: {}
+        // Respond from the cache if we can.  Our index.html
+        // never changes, so we're OK to cache it in perpetuity.
+        if (cachedResponse){
+            res.send(cachedResponse);
+            return;
+        }
+
+        // Else render index.html
+        res.render('index.html', {meetups: {} }, function(err, html){
+            cachedResponse = html;
+            res.send(cachedResponse);
+            return;
         });
     });
 
