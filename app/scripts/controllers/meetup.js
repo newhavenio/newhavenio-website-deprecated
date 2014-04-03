d86a9c933fb246cbe3abf4c920680d6a0eb0a9bb
Joel Nimety <jnimety@continuity.net>
2014-04-03 19:00:45 -0400

    remove A100 feed from homepage

--- app/scripts/controllers/meetup.js
+++ app/scripts/controllers/meetup.js
@@ -12,10 +12,6 @@ angular.module('nhvioApp')
       {
         "eventPrefix": "",
         "meetupUrl": "http://api.meetup.com/2/events?callback=JSON_CALLBACK&status=upcoming&order=time&limited_events=False&group_urlname=newhavenio&desc=false&offset=0&format=json&page=20&fields=&sig_id=26187422&sig=aa008c5591ab44f404d9c70a48211e9089e5f7f8"
-      },
-      {
-        "eventPrefix": "A100",
-        "meetupUrl": "http://api.meetup.com/2/events?callback=JSON_CALLBACK&status=upcoming&order=time&limited_events=False&group_urlname=a100-dev-community&desc=false&offset=0&format=json&page=10&fields=&sig_id=26187422&sig=eb79b98cfd0843a7ad2998077697ca2f2d6baf76"
       }
     ];
     $scope.events = meetupService.getEvents(feeds);
